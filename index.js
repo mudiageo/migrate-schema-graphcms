6ad9057853130460d9526915d191fccab3d92bb4
mudiageo <mudiageo@gmail.com>
2022-05-29 19:44:51 +0100

    Added index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-﻿
+const { newMigration, FieldType, Renderer, RelationType } = require("@graphcms/management");
+
+const migration = newMigration({ endpoint: "...", authToken: "..." });
+
+const author = migration.createModel({
+  apiId: "Author",
+  apiIdPlural: "Authors",
+  displayName: "Author",
+});
+
+author.addSimpleField({
+  apiId: "firstName",
+  displayName: "First Name",
+  type: FieldType.String,
+});
+author.addSimpleField({
+  apiId: "lastName",
+  displayName: "Last Name",
+  type: FieldType.String,
+});
+
+migration.run();
